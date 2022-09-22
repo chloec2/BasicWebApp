d03303e87a78ce667869c317549cc0afabebb8e0
Chloe Chan <chloec@andrew.cmu.edu>
2022-09-22 10:42:14 -0400

    test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,7 +9,7 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
         if (query.toLowerCase().contains("name")) {
-            return "hi";
+            return "hi"; // change
         }
         if (query.toLowerCase().contains("chair")) {
             return "a thing to sit on";
